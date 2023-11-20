31f4220b659b21b8d58fbf47217705a23fd73731
Natalia Kimel <nata@rus9n.com>
2023-11-20 23:01:00 +0300

    final

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Natalia-Kimel/mesto-project-ff/tree/main
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(item, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__image').alt = item.name;
+    cardElement.querySelector('.card__title').textContent = item.name;
+
+    const deleteCardButton = cardElement.querySelector('.card__delete-button');
+    deleteCardButton.addEventListener('click', deleteCard);
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    const deletedCard = evt.target.closest('.places__item');
+    deletedCard.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function addCard(item) {
+    placesList.append(item);
+}
+
+initialCards.forEach((item) => {
+    const newCard = createCard(item, deleteCard);
+    addCard(newCard);
+});
